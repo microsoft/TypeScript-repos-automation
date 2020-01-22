7e2a2834e1d9a07be1ec327d02d66fe8f3be1fec
Orta Therox <orta.therox@gmail.com>
2020-01-22 10:42:55 -0500

    Adds tests to cover my last bug

---
+++ src/tests/TypeScriptRepoPullRequestWebhook.test.ts
@@ -0,0 +1,10 @@
+jest.mock("../typeScriptHandlePullRequest", () => ({ handlePullRequestPayload: jest.fn() }))
+import webhook from "../../TypeScriptRepoPullRequestWebhook/index"
+import { handlePullRequestPayload } from "../typeScriptHandlePullRequest"
+
+it("calls handle PR from the webhook main", () => {
+  process.env.AZURE_FUNCTIONS_ENVIRONMENT = "Development"
+  webhook({} as any, { body: {}})
+
+  expect(handlePullRequestPayload).toHaveBeenCalled()
+})
