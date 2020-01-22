7b2bff932b243e717e00bcbbce09314064725632
Orta Therox <orta.therox@gmail.com>
2020-01-22 11:06:20 -0500

    Fix the github API check

--- src/util/createGitHubClient.ts
+++ src/util/createGitHubClient.ts
@@ -2,7 +2,7 @@ import * as Octokit from "@octokit/rest"
 
 const inTests = typeof jest !== "undefined"
 if (!inTests) {
-  if (!process.env.GITHUB_TOKEN || !process.env.GITHUB_API_TOKEN) throw new Error(`There isn't a GITHUB_API_TOKEN in your env, found: ${Object.keys(process.env)}`)
+  if (!process.env.GITHUB_TOKEN && !process.env.GITHUB_API_TOKEN) throw new Error(`There isn't a GITHUB_API_TOKEN in your env, found: ${Object.keys(process.env)}`)
 }
 
 export const createGitHubClient = () => 
