98e747e776fa87e0acfcbb1de0bbe13fafa11d30
Orta Therox <orta.therox@gmail.com>
2020-01-22 11:14:45 -0500

    Log payload

--- src/typeScriptHandlePullRequest.ts
+++ src/typeScriptHandlePullRequest.ts
@@ -10,12 +10,15 @@ import {sha} from "./sha"
 export const handlePullRequestPayload = async (payload: WebhookPayloadPullRequest, context: Context) => {
   const api = createGitHubClient();
   const ran = [] as string[]
+
   const run = (name: string, fn: (api: Octokit, payload: WebhookPayloadPullRequest, logger: Logger) => Promise<void>) => {
     context.log.info(`\n\n## ${name}\n`)
     ran.push(name)
     return fn(api, payload, context.log)
   }
   
+  context.log.info("payload", Object.keys(payload))
+
     // Run checks
   await run("Assigning Self to Core Team PRs", assignSelfToNewPullRequest)
   await run("Add a core team label to PRs", addLabelForTeamMember)
