b5095a26ac7d32b64adcfee37861cb33e9c12ba1
Orta Therox <orta.therox@gmail.com>
2020-01-22 11:36:52 -0500

    Make green

--- src/tests/TypeScriptRepoPullRequestWebhook.test.ts
+++ src/tests/TypeScriptRepoPullRequestWebhook.test.ts
@@ -4,7 +4,7 @@ import { handlePullRequestPayload } from "../typeScriptHandlePullRequest"
 
 it("calls handle PR from the webhook main", () => {
   process.env.AZURE_FUNCTIONS_ENVIRONMENT = "Development"
-  webhook({} as any, { body: {}})
+  webhook({} as any, { body: "{}"})
 
   expect(handlePullRequestPayload).toHaveBeenCalled()
 })
