8d1a306f385e4f22a6cda8a61556b71eadbd2709
Orta Therox <orta.therox@gmail.com>
2020-01-22 16:29:21 -0500

    Better not be OS specific for th EOL

--- src/pr_meta/isMemberOfTSTeam.ts
+++ src/pr_meta/isMemberOfTSTeam.ts
@@ -1,10 +1,11 @@
 import Octokit = require("@octokit/rest");
 import type { Logger } from "@azure/functions";
-import {EOL} from "os"
 let cachedTSTeam: string [] = []
 
 /** Checks if someone is a member of a team, and always bails with TS bot */
 export const isMemberOfTSTeam = async (username: string, api: Octokit, log: Logger) => {
+  log.info("Found: ", cachedTSTeam.join(", "))
+
   if (username === "typescript-bot") return false;
   // Keep a cache so that it's only grabbed every so often
   if (cachedTSTeam.length) {
@@ -16,8 +17,7 @@ export const isMemberOfTSTeam = async (username: string, api: Octokit, log: Logg
   
   // @ts-ignore types are mismatched
   const usersText = Buffer.from(contentResponse.data.content, 'base64').toString()
-  cachedTSTeam = usersText.split(EOL)
-  log.info("Found: ", cachedTSTeam.join(", "))
+  cachedTSTeam = usersText.split("\n")
 
   return cachedTSTeam.includes(username)
 }
