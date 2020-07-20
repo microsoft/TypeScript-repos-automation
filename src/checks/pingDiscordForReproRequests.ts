c0434ab2b00f4a9c93651bffbe5da2311d30a911
Orta <git@orta.io>
2020-07-20 13:43:36 -0400

    Adds JSON header to the discord bot

--- src/checks/pingDiscordForReproRequests.ts
+++ src/checks/pingDiscordForReproRequests.ts
@@ -9,30 +9,36 @@ import fetch from "node-fetch"
 export const pingDiscordForReproRequests = async (api: Octokit, payload: WebhookPayloadIssues, logger: Logger) => {
   const { issue } = payload
 
-  const label = (payload as any).label 
+  const label = (payload as any).label
   if (label && label.name && label.name === "Repro Requested") {
     if (!process.env.REPRO_REQUEST_DISCORD_WEBHOOK) throw new Error("No process var for REPRO_REQUEST_DISCORD_WEBHOOK")
 
     // https://discord.com/developers/docs/resources/webhook#execute-webhook
-    
+
     let body = issue.body.slice(0, 199)
     // The template has comments which might have been kept in
     if (issue.body.includes("𝗦𝗧𝗢𝗣") && issue.body.includes("## Use Cases")) {
       // https://regex101.com/r/uEmvAZ/1
       const stripComments = /<!--[^>]*-->/g
       body = issue.body.replace(stripComments, "").split("## Use Cases")[1].split("## Checklist")[0].slice(0, 199)
-    } 
+    }
 
     const webhook = {
       content: `Repro requested on #${issue.number}`,
-      embeds: [{
-        title: issue.title,
-        description: body.length === 200 ? body + "..." : body,
-        url: issue.html_url
-      }]
+      embeds: [
+        {
+          title: issue.title,
+          description: body.length === 200 ? body + "..." : body,
+          url: issue.html_url,
+        },
+      ],
     }
 
     logger.info("Sending Discord ping")
-    fetch(process.env.REPRO_REQUEST_DISCORD_WEBHOOK, { method: "POST", body: JSON.stringify(webhook) })
+    fetch(process.env.REPRO_REQUEST_DISCORD_WEBHOOK, {
+      method: "POST",
+      body: JSON.stringify(webhook),
+      headers: { "Content-Type": "application/json" },
+    })
   }
 }
