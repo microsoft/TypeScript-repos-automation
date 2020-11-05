40ebe7fd867e6363bd73e0ce39d5cc92cdfd9708
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2020-11-05 09:16:47 -0800

    Polish addCommentToUncommittedPRs
    1. Clean up dead code/comments.
    2. Don't comment on merged PRs.
    3. Improve wording a little.

--- src/checks/addCommentToUncommittedPRs.ts
+++ src/checks/addCommentToUncommittedPRs.ts
@@ -9,24 +9,23 @@ import { isMemberOfTSTeam } from "../pr_meta/isMemberOfTSTeam"
  */
 export const addCommentToUncommittedPRs = async (api: Octokit, payload: WebhookPayloadPullRequest, logger: Logger) => {
   const { repository: repo, pull_request } = payload
-  pull_request.number
+  if (pull_request.merged) {
+    return
+  }
 
   const relatedIssues = await getRelatedIssues(pull_request.body, repo.owner.login, repo.name, api)
+
   const thisIssue = {
     repo: repo.name,
     owner: repo.owner.login,
     issue_number: pull_request.number,
   }
-    // if no related issues, comment "please open an issue first. we'll prioritise and discuss there"
-    // if related issue(s) have no tag like Exp Enhancement, Accepted, Help Wanted, Milestone: X, Backlog, comment "watch out, we may not take this. make sure the issue is accepted first"
-    // note: all these are technically exempt for core team members, although they should know better, so
-
   if (!relatedIssues || relatedIssues.length === 0) {
     const comments = (await api.issues.listComments(thisIssue)).data
     if (!comments || !comments.find(c => c.body.startsWith('The PR doesn'))) {
       await api.issues.createComment({
         ...thisIssue,
-        body: "The PR doesn't have any linked issues. Please open an issue that references this PR. From there we can discuss and prioritise."
+        body: "This PR doesn't have any linked issues. Please open an issue that references this PR. From there we can discuss and prioritise."
       })
     }
   }
@@ -38,7 +37,7 @@ export const addCommentToUncommittedPRs = async (api: Octokit, payload: WebhookP
       if (!comments || !comments.find(c => c.body.startsWith('The TypeScript team has'))) {
         await api.issues.createComment({
           ...thisIssue,
-          body: `The TypeScript team hasn't accepted the linked issue #${relatedIssues[0].number}. This makes it less likely that we'll review or accept this PR. Try to get the originating issue accepted.`
+          body: `The TypeScript team hasn't accepted the linked issue #${relatedIssues[0].number}. If you can get it accepted, this PR will have a better chance of being reviewed.`
         })
       }
     }
