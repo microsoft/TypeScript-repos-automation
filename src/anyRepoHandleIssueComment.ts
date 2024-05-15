b2ddb4d664403ecfa1d3110d2a35c6fd83084472
Jake Bailey <5341706+jakebailey@users.noreply.github.com>
2024-05-14 20:43:44 -0700

    Remove code which allows merging of PRs (#52)

--- src/anyRepoHandleIssueComment.ts
+++ src/anyRepoHandleIssueComment.ts
@@ -3,7 +3,6 @@ import { HttpResponseInit, InvocationContext } from "@azure/functions"
 import { createGitHubClient } from "./util/createGitHubClient"
 import { Octokit } from "@octokit/rest"
 import { sha } from "./sha"
-import { mergeThroughCodeOwners } from "./checks/mergeThroughCodeOwners"
 import { addReprosLabelOnComments } from "./checks/addReprosLabel"
 import { Logger } from "./util/logger"
 
@@ -18,11 +17,6 @@ export const anyRepoHandleIssueCommentPayload = async (payload: IssueCommentEven
     context.info(`\n\n## ${name}\n`)
     ran.push(name)
     return fn(api, payload, context)
-  }
-
-  // Making this one whitelisted to the website for now
-  if (payload.repository.name === "TypeScript-Website") {
-    await run("Checking if we should merge from codeowners", mergeThroughCodeOwners)
   }
 
   if (payload.repository.name === "TypeScript") {
--- src/util/getContents.ts
+++
@@ -1,8 +0,0 @@
-import { Octokit, RestEndpointMethodTypes } from "@octokit/rest"
-
-export const getContents = async (api: Octokit, opts: RestEndpointMethodTypes["repos"]["getContent"]["parameters"]) => {
-  const contentResponse = await api.repos.getContent(opts)
-  // @ts-ignore types are mismatched
-  const text = Buffer.from(contentResponse.data.content, "base64").toString()
-  return text
-}
